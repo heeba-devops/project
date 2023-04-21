6a9f80c2231daa0bb8711de5eafc693456144d60
edureka <support@edureka.co>
2023-04-21 16:44:15 +0000

    Revert "Modified Simple.java"
    This reverts commit 2edc880525a8bcbcb4efee83269b9c0f4d55f632.

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
 // Print Message on the screen
 class Simple {
     public static void main (String args[]) {
-    System.out.println("Welcome to Jenkins Online Trainig!");
+    System.out.println("Welcome to Edureka!");
     }
 
 }
