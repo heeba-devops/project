81ac8e8f7e1291e17d30edeb117b969e024b3840
heeba-devops <131389878+heeba-devops@users.noreply.github.com>
2023-05-16 20:24:23 +0400

    changed

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
 // Print Message on the screen
 class Simple {
     public static void main (String args[]) {
-    System.out.println("Welcome to Edureka!");
+    System.out.println("Welcome!");
     }
 
 }
